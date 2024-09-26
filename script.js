082752facf73fc69a28ed1c62399111ca3fc43ca
Cynario4eva <joshdela233@gmail.com>
2024-09-26 13:25:16 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.btn');
+let notes = document.querySelectorAll('.input-box');
+
+
+function showNotes(){
+  notesContainer.innerHTML = localStorage.getItem('note-data')
+}
+
+showNotes();
+
+
+function updateStorage(){
+  localStorage.setItem('note-data', notesContainer.innerHTML);
+}
+
+
+createBtn.addEventListener('click',()=>{
+  let inputBox = document.createElement('p');
+  let img = document.createElement('img');
+  inputBox.classList.add('input-box');
+  inputBox.setAttribute('contenteditable','true');
+  img.src = 'images/delete.png';
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+
+notesContainer.addEventListener('click',(e)=>{
+  if(e.target.tagName === 'IMG'){
+    e.target.parentElement.remove();
+    updateStorage();
+  }else {
+    if(e.target.tagName === 'P'){
+      notes = document.querySelectorAll('.input-box');
+      notes.forEach(note =>{
+             note.onkeyup = ()=> updateStorage();
+      });
+    }
+  }
+});
+
+document.addEventListener('keydown',e => {
+  if(e.key === 'Enter'){
+    document.execCommand('insertLineBreak');
+    e.preventDefault();
+  }
+})
